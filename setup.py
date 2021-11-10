08a3230619ed5d5fab33bee3d2c97494bec6f834
Gaetan Manchon <gmanchon@gmail.com>
2021-11-10 15:45:35 +0100

    increase package minor version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 requirements = [x.strip() for x in content]
 
 setup(name="myriad",
-      version="0.1",
+      version="0.2.0",
       description="Le Wagon challenge split tool",
       packages=find_packages(),
       install_requires=requirements,
