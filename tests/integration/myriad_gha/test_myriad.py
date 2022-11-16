9d3f1e363b3ee2451b59a928d651d3841fb5b4c8
Gaetan Manchon <gmanchon@gmail.com>
2022-11-16 20:45:08 +0100

    initialise repo with empty commit

--- tests/integration/myriad_gha/test_myriad.py
+++ tests/integration/myriad_gha/test_myriad.py
@@ -66,7 +66,7 @@ class TestMyriadGha():
 
         solutions_repo.init()
         solutions_repo.add()
-        solutions_repo.commit(message="initial commit")
+        solutions_repo.commit(message="initial commit", allow_empty=True)
         solutions_repo.remote_add(gh_solutions_repo)
         solutions_repo.push()
 
