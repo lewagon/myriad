2cacc0375d5040bea088f7128019acecf00ad984
Gaetan Manchon <gmanchon@gmail.com>
2023-09-13 12:56:42 +0200

    add unit test boilerplate

---
+++ tests/unit/gen/test_context.py
@@ -0,0 +1,19 @@
+
+from wagon_myriad.github.context import get_challenge_root
+
+
+class TestContext():
+
+    def test_challenge_path(self):
+        """
+        test that the path of the challenge of any file is correctly retrieved
+        by looking at the location of the `.lewagon/metadata.yml` metadata file
+        """
+
+        # Arrange
+
+        # Act
+
+        # Assert
+
+        # Cleanup
